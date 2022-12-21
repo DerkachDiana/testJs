dfbaa71acfb9bf9fc80d2fd9fa56ef9e59694dc9
qwerty <qwerty@mail.ru>
2022-12-21 19:56:25 +0300

    Add albums

--- scripts/albums.js
+++ scripts/albums.js
@@ -1,27 +1,85 @@
+async function albumContainerHandler(elements) {
+    if(!elements.closest('.button-show')) {
+        return;
+    }
+
+    const albumContainer = elements.closest('.albums-container');
+    const photosContainer = elements.parentNode.parentNode.querySelector('.photos-container');
+    const buttonShow = elements.closest('.button-show');
+    const album = buttonShow.parentNode;
+    const parentsContainerName = buttonShow.parentNode.parentNode.className;
+
+    if(parentsContainerName === 'albums-container') {
+        setShowLessButton(buttonShow); 
+    }
+    
+    if(photosContainer){
+        photosContainer.remove();
+    }
+
+    if(elements.closest('.button-show--less') && !photosContainer && parentsContainerName === 'albums-container') {
+        console.log(album.id)
+        const photos = await getPhotos(album.id);
+        console.log(photos)
+        const photosContainer = renderPhotos(photos)
+
+        albumContainer.appendChild(photosContainer);
+    }    
+}
+
+function renderPhotos(photos) {
+    console.log('one');
+   const photosContainer = document.createElement('div');
+
+   photosContainer.className = 'photos-container';
+  
+   photos.forEach(photo => {
+       const photoItem = renderPhotoItem(photo);
+
+       photosContainer.appendChild(photoItem);
+   });
+
+   return photosContainer;
+}
+
+function renderPhotoItem(photo) {
+    const photoElement = document.createElement('img');
+    photoElement.className = 'photo-item';
+    photoElement.src = photo.url;
+
+    return photoElement;
+}
+
+
+function renderAlbumItem(album) {
+    const albumItem = document.createElement('div');
+    const showButton = renderShowButton()
+    const albumTitle = document.createElement('div');
+
+    albumItem.id = album.id;
+
+    albumItem.classList.add('rendered-item');
+    albumItem.classList.add('album-padding');
+
+    albumTitle.innerHTML = album.title;
+
+    albumItem.appendChild(showButton);
+    albumItem.appendChild(albumTitle);
+
+    return albumItem;
+}
+
 async function renderAlbums(userContainer) {
     const albums = await getAlbums(userContainer.id);
-    console.log(albums);
     const albumsContainer = document.createElement('div');
 
+    albumsContainer.className = 'albums-container';
+
     albums.forEach((album) => {
-        const albumItem = document.createElement('div');
-        const showButton = document.createElement('div');
-        const albumTitle = document.createElement('div');
-
-        albumItem.id = album.id;
-
-        showButton.className = 'button-show';
-        showButton.classList.add('button-show--more');
-
-        albumTitle.innerHTML = album.title;
-        console.log(albumTitle);
-        albumItem.appendChild(showButton);
-        albumItem.appendChild(albumTitle);
-        // albumsContainer.appendChild(albumContainer);
-
-        userContainer.appendChild(albumItem);
+        const albumItem = renderAlbumItem(album);
+        albumsContainer.appendChild(albumItem);
     })
 
-    
-    console.log(userContainer);
-}
+    userContainer.appendChild(albumsContainer);
+
+    }
